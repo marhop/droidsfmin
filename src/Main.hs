813954f9d33fb3eb1faafbef8d4f93cb05a5a329
Martin Hoppenheit <martin@hoppenheit.info>
2016-06-22 11:22:14 +0200

    Read STDIN if no file supplied.

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,7 @@ import DroidSignatureFileFilter
 
 -- | Construct a usage message header.
 header :: String -> String
-header progName = "Usage: " ++ progName ++ " [options] [arguments]"
+header progName = "Usage: " ++ progName ++ " [options] [signature file]"
 
 -- | Data type for command line options.
 data Options = Options
@@ -16,7 +16,7 @@ data Options = Options
     , optPuids :: [String]
     } deriving (Show)
 
--- | Default vaules command line options.
+-- | Default vaules for command line options.
 defaultOptions :: Options
 defaultOptions = Options
     { optHelp  = False
@@ -43,14 +43,17 @@ parseArgs hdr argv =
         (opts, args, []) -> return (foldr id defaultOptions opts, args)
         (_, _, errs)     -> error $ concat errs ++ usageInfo hdr options
 
+-- | Read from a file or from STDIN if no file is specified.
+input :: [FilePath] -> IO String
+input []    = getContents
+input (f:_) = readFile f
+
 main = do
     prg <- getProgName
     (opts, args) <- getArgs >>= parseArgs (header prg)
     when (optHelp opts) $ do
         putStr $ usageInfo (header prg) options
         exitSuccess
-    -- TODO Default to STDIN if no file supplied.
-    when (not $ null args) $ do
-        content <- readFile $ head args
-        putStrLn $ filterSigFile (optPuids opts) content
+    content <- input args
+    putStrLn $ filterSigFile (optPuids opts) content
 
