00c63b308d3503385bef90683b745f76f909959b
Martin Hoppenheit <martin@hoppenheit.info>
2016-06-22 12:02:55 +0200

    Add command line option to specify output file.
    And re-organize command line parsing a little bit.

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import System.Environment (getProgName, getArgs)
 import System.Console.GetOpt
 import System.Exit
 import Control.Monad (when)
+import Data.Maybe (listToMaybe)
 import DroidSignatureFileFilter
 
 -- | Construct a usage message header.
@@ -12,15 +13,17 @@ header progName = "Usage: " ++ progName ++ " [options] [signature file]"
 
 -- | Data type for command line options.
 data Options = Options
-    { optHelp  :: Bool
-    , optPuids :: [String]
+    { optHelp    :: Bool
+    , optPuids   :: [String]
+    , optOutFile :: Maybe FilePath
     } deriving (Show)
 
 -- | Default vaules for command line options.
 defaultOptions :: Options
 defaultOptions = Options
-    { optHelp  = False
-    , optPuids = []
+    { optHelp    = False
+    , optPuids   = []
+    , optOutFile = Nothing
     }
 
 -- | Description of the command line options and how to merge the supplied
@@ -33,27 +36,35 @@ options =
     , Option ['p'] ["puid"]
         (ReqArg (\p opts -> opts { optPuids = p:(optPuids opts) }) "PUID")
         "PUID to include in the output"
+    , Option ['o'] ["output"]
+        (ReqArg (\o opts -> opts { optOutFile = Just o }) "FILE")
+        "output file"
     -- TODO Add -P option to read PUIDs from file.
     ]
 
 -- | Parse command line options.
-parseArgs :: String -> [String] -> IO (Options, [String])
+parseArgs :: String -> [String] -> IO (Options, Maybe FilePath)
 parseArgs hdr argv =
     case getOpt Permute options argv of
-        (opts, args, []) -> return (foldr id defaultOptions opts, args)
-        (_, _, errs)     -> error $ concat errs ++ usageInfo hdr options
+        (os, as, []) -> return (foldr id defaultOptions os, listToMaybe as)
+        (_, _, es)   -> error $ concat es ++ usageInfo hdr options
 
 -- | Read from a file or from STDIN if no file is specified.
-input :: [FilePath] -> IO String
-input []    = getContents
-input (f:_) = readFile f
+input :: Maybe FilePath -> IO String
+input (Just f) = readFile f
+input Nothing  = getContents
+
+-- | Write to a file or to STDOUT if no file is specified.
+output :: Maybe FilePath -> String -> IO ()
+output (Just f) = writeFile f
+output Nothing  = putStrLn
 
 main = do
     prg <- getProgName
-    (opts, args) <- getArgs >>= parseArgs (header prg)
+    (opts, file) <- getArgs >>= parseArgs (header prg)
     when (optHelp opts) $ do
         putStr $ usageInfo (header prg) options
         exitSuccess
-    content <- input args
-    putStrLn $ filterSigFile (optPuids opts) content
+    content <- input file
+    output (optOutFile opts) $ filterSigFile (optPuids opts) content
 
